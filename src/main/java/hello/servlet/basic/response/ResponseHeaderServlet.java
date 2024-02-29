de4fc0817a9ec045aa7e9f144fe75c41a357318f
kimdodo97 <eh4536@naver.com>
2024-02-29 16:20:28 +0900

    feat : 서블릿 응답 기능 추가

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,56 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name="reponseHeaderServlet",urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //[status line]
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        //[reponse headers]
+        response.setHeader("Content-Type","text/plain;charset=utf-8");
+        response.setHeader("Cache-Control","no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma","no-cache");
+        response.setHeader("my-header","hello");
+
+        //[Header 편의 메서드]
+        //content(response);
+        //cookie(response);
+
+        redirect(response);
+        response.getWriter().write("안녕");
+    }
+
+    private void content(HttpServletResponse response){
+        //Content-Type : text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type","text/plain;charset=utf-8")
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2);
+    }
+
+    private void cookie(HttpServletResponse response){
+        Cookie cookie = new Cookie("myCookie","good");
+        cookie.setMaxAge(600);//600초
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException{
+        //status code 302
+        //Location: /basic/hello-form.html
+
+//        response.setStatus(HttpServletResponse.SC_FOUND);
+//        response.setHeader("Location","/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+    }
+}
