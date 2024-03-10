1d2f852fe541e1e893069066a6e9b19bbdfc8a88
kimdodo97 <eh4536@naver.com>
2024-03-10 22:49:54 +0900

    feat : 프론트컨트롤러 뷰, 모델 구현

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -4,7 +4,7 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.util.Map;
 import java.io.IOException;
 
 public class MyView {
@@ -17,5 +17,15 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request,response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
