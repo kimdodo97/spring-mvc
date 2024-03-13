fe3c5d7928ed1f89b4f55bde6f3769a1d4c90538
kimdodo97 <eh4536@naver.com>
2024-03-14 00:47:07 +0900

    feat : 실습 코드 추가

--- src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -1,10 +1,9 @@
-package hello.servlet.web.frontcontroller.V4;
+package hello.servlet.web.frontcontroller.v4;
 
-import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.V4.controller.MemberFormControllerV4;
-import hello.servlet.web.frontcontroller.V4.controller.MemberListControllerV4;
-import hello.servlet.web.frontcontroller.V4.controller.MemberSaveControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
